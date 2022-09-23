2a39b1e5bccca4333d1df45568635ca31eb01cc0
Jesse Maraya <jessemaraya2013@gmail.com>
2022-09-22 18:55:40 -0700

    create api keys for quiz, created home

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,11 +1,25 @@
-// import React from "react";
+import React from "react";
 
 
 
-// function Quiz() {
+function Quiz() {
 
-// <div>Hello</div>>
+    // apiKey=ws2ucoct0wippBIoqt7JwUpsbmDq0TbZSQkwFvD7
 
-// }
+    // 10 easy 
+    // https://quizapi.io/api/v1/questions?apiKey=ws2ucoct0wippBIoqt7JwUpsbmDq0TbZSQkwFvD7&difficulty=Easy&limit=10&tags=HTML,JavaScript,MySQL
 
-// export default Quiz;
+    // 10 Medium 
+    // https://quizapi.io/api/v1/questions?apiKey=ws2ucoct0wippBIoqt7JwUpsbmDq0TbZSQkwFvD7&difficulty=Medium&limit=10&tags=HTML,JavaScript,MySQL
+
+    // 10 Hard 
+    // https://quizapi.io/api/v1/questions?apiKey=ws2ucoct0wippBIoqt7JwUpsbmDq0TbZSQkwFvD7&difficulty=Hard&limit=10&tags=HTML,JavaScript,MySQL
+
+return(
+    <h1>Hello it's the quiz</h1>
+);
+
+
+}
+
+export default Quiz;
